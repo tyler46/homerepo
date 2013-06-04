4551341c8d5ccef306bad68a209dbdf68f276612
spyros <spyros@movinapp.com>
2013-06-04 15:02:38 +0300

    Starting project

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ HomeRepo Documentation
 Server for HomeRepo
 -------------------
 
-homerepo is an application used in order to manage the sharing amenities among the tenants of an appartment building
+homerepo is an application used in order to manage the sharing amenities among the tenants of an appartment building.
---
+++ homerepo/settings/production.py
@@ -0,0 +1,14 @@
+# Normally you should not import ANYTHING from Django directly
+# into our settings, but ImproperlyConfigured is an exception.
+from django.core.exceptions import ImproperlyConfigured
+
+
+def get_env_variable(var_name):
+    """Gets the environment variable or returns exception."""
+    try:
+        return os.environ[var_name]
+    except KeyError:
+        error_msg = "Set the %s enviroment variable" % var_name
+        raise ImproperlyConfigured(error_msg)
+
+SECRET_KEY = get_env_variable('HOMEREPO_SECRET_KEY')
