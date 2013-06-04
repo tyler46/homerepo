99c30d1845962eb03279bd998f531bc1b681d7b3
tyler46 <mail.doctor46@gmail.com>
2013-06-04 19:11:10 +0300

    Define static & media folders

--- homerepo/settings/base.py
+++ homerepo/settings/base.py
@@ -1,4 +1,11 @@
 # Django settings for homerepo project.
+import os
+
+here = lambda path: os.path.join(os.path.abspath(os.path.dirname(__file__)), path)
+
+PROJECT_ROOT = here('../..')
+
+root = lambda path: os.path.join(os.path.abspath(PROJECT_ROOT), path)
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -38,18 +45,18 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/var/www/example.com/media/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = root('media/')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://example.com/media/", "http://media.example.com/"
-MEDIA_URL = ''
+MEDIA_URL = '/media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = root('static/')
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
@@ -106,9 +113,10 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+    'south',
 )
 
 # A sample logging configuration. The only tangible logging
